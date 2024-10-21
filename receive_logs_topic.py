902b17316ef9303400a89f924d09c3f0c786e277
Eros God <erosgod19921118@gmail.com>
2024-10-21 10:01:39 -0400

    Implement topic exchange by FMRGJ.

--- receive_logs_topic.py
+++ receive_logs_topic.py
@@ -4,20 +4,20 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel() 
 
-channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
 
 result = channel.queue_declare(queue='', exclusive=True)
 queue_name = result.method.queue 
 
-severities = sys.argv[1:]
-if not severities:
-    sys.stderr.write("Usage: %s [info] [warning] [error] \n" % sys.argv[0])
+binding_keys = sys.argv[1:]
+if not binding_keys:
+    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
     sys.exit(1)
 
-for severity in severities:
-    channel.queue_bind(exchange='direct_logs', queue=queue_name, routing_key=severity)
+for binding_key in binding_keys:
+    channel.queue_bind(exchange='topic_logs', queue=queue_name, routing_key=binding_key)
 
-print(" [x] Waiting for logs. To exit, press CTRL+C")
+print(f" [x] Waiting for logs. To exit, press CTRL+C")
 
 def callback(ch, method, properties, body):
     print(f" [x] {method.routing_key}:{body}")
